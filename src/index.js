5b7d3248bf137be132c4e98758832e8a98615cc4
bluefishhh93 <xuanthanha03@gmail.com>
2024-05-07 13:41:06 +0700

    excircises 2

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const rect = {
+    perimeter: (x, y) => 2 * (x + y),
+    area: (x, y) => x * y
+};
+
+function solveRect(length, breadth) {
+    console.log(`Solving for rectangle with l = ${length} and b = ${breadth}`);
+
+    if (length <= 0 || breadth <= 0) {
+        console.log(`Rectangle dimensions should be greater than zero: l = ${length}, and b = ${breadth}`);
+    } else {
+        console.log(`The area of the rectangle is ${rect.area(length, breadth)}`);
+        console.log(`The perimeter of the rectangle is ${rect.perimeter(length, breadth)}`);
+    }
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
